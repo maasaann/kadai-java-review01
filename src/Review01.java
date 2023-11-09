8e59c2cca7b107eb0c07dfc167ce12d380f48c23
yoshitoshi.​miyata <meyutana@gmail.com>
2023-11-09 20:15:17 +0900

    再提出分。taxメソッドへ修正しました

--- src/Review01.java
+++ src/Review01.java
@@ -7,15 +7,15 @@ public class Review01 {
 
         double tax_rate = 0.1; //税率
 
-        int tax = tax(price,tax_rate); //税額計算
+        int tax_price = tax(price,tax_rate); //税額計算
 
-        int total = price + tax;
+        int total = price + tax_price; //総額
 
-        System.out.println(price + "円の商品の税込価格は" + total + "円（消費税は" + tax + "円）です。");
+        System.out.println(price + "円の商品の税込価格は" + total + "円（消費税は" + tax_price + "円）です。");
 
     }
 
-    //taxメソッド
+    //taxメソッj
     public static int tax(int price,double tax_rate) {
         int result = ((int)(price * tax_rate));
         return result;
